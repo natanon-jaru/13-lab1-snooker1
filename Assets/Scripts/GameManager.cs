a3dc9e7a0fbeecd2f360ad476512a40ea67d80d6
natanon-jaru <natanon.jari@bumail.net>
2023-11-23 16:36:56 +0700

    set ball and camera to play

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
-    [SerializeField] private int PlayerScore; 
+    [SerializeField] private int playerScore; 
     [SerializeField] private GameObject ballPrefab;
     [SerializeField] private GameObject[] ballPositions;
+
+    [SerializeField] private GameObject cueBall;
+    [SerializeField] private GameObject ballLine;
+
+    [SerializeField] private float xInput;
+    [SerializeField] private float force;
+
+    [SerializeField] private GameObject camera;
     
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
+
+        camera = Camera.main.gameObject;
+        CameraBehindBall();
         
         //set balls on the table
         SetBalls(BallColors.White, 0);
@@ -29,7 +41,17 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        RotateBall();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ShootBall();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            StopBall();
+        }
     }
 
     void SetBalls(BallColors color, int pos)
@@ -39,5 +61,39 @@ public class GameManager : MonoBehaviour
                                     Quaternion.identity);
         Ball b = ball.GetComponent<Ball>();
         b.SetColorAndPoint(color);
+    }
+
+    void RotateBall()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        cueBall.transform.Rotate(new Vector3(0f,xInput/10,0f));
+    }
+
+    void ShootBall()
+    {
+        camera.transform.parent = null;
+        Rigidbody rd = cueBall.GetComponent<Rigidbody>();
+        rd.AddRelativeForce(Vector3.forward * force, ForceMode.Impulse);
+        ballLine.SetActive(false);
+    }
+
+    void CameraBehindBall()
+    {
+        camera.transform.parent = cueBall.transform;
+        camera.transform.position = cueBall.transform.position + new Vector3(0f, 20f, -15f);
+    }
+
+
+    void StopBall()
+    {
+        Rigidbody rd = cueBall.GetComponent<Rigidbody>();
+        rd.velocity = Vector3.zero;
+        rd.angularVelocity = Vector3.zero;
+        
+        cueBall.transform.eulerAngles = Vector3.zero;
+        CameraBehindBall();
+        camera.transform.eulerAngles = new Vector3(40f, 0f,0f);
+        ballLine.SetActive(true);
+        
     }
 }
